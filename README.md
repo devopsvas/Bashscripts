95f46fffcde6e1e458bf3e70c94f7d8ab4395d01
root <root@localhost.localdomain>
2021-08-29 00:01:12 +0530

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Bashscripts
+
+Simple shell script to backup mysql DB locally.
+Location can be used with NFS or any other means of storage for remote back.
+user script in the corn for sheduling the backup.
+
---
+++ mysqlbackup.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# Written by Vasanth.T.M
+# Copy write 2010
+
+
+CKUPDATE=$(date +%d-%m-%Y)
+BACKUPDIR=/mysqlbackup
+DATABASES=$(mysql -u root -h localhost -pmysql -Bse 'show databases')
+
+
+delete_old ()
+{
+echo Deleting old backup of backup of "$name"
+name="$1"
+find "$BACKUPDIR" -name "$name-*.sql.bz2" | sort | head -n -2 | xargs --no-run-if-empty rm -f
+}
+
+back ()
+{
+for GH in $DATABASES; do
+echo "Creating mysql backup of $GH"
+mysqldump -u root -pmysql $GH | bzip2 --compress --stdout > $BACKUPDIR/$GH-$CKUPDATE.sql.bz2
+name=`basename $GH`
+delete_old "$name"
+done
+}
+back
